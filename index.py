e65e9097951b47f85a84ae88768bc2bdb2afcf41
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-11-25 18:13:30 +0500

    les9

---
+++ index.py
@@ -0,0 +1,20 @@
+# name = 'Zokhir and shaxa'
+# name = name.upper()
+# name = name.lower()
+#index = kakoy index simvola dobustim bukva "s" (index, start, end)
+# .isalpha bukva ili ne bukva 
+#count poschetaayet kolichestvo bukv ili sifr
+#find esli ne noxodit to -1 
+#strip - ub 
+
+# str = 'ebesayuxen'
+# str2 = ''
+# for ltr in str:
+#     str2 = ltr + str2
+#     print(str2)
+
+# randomizer **//
+# from random import randint, random 
+
+# print(randint(0, 255))
+
